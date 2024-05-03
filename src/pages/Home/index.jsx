b0af6541808e7b0c97105fd4fb7e4a461df9d6c1
millena-softsy <millena.souza@softsy.io>
2024-05-03 19:13:58 -0300

    Componente buttonMenu feito, tela de home sendo finalizada (faltando apenas linkar com as rotas)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,31 @@
 import './home.css'
 import { FaBars } from "react-icons/fa";
 import logo from '../../assets/logo-sumare.svg'
+import notebook from '../../assets/notebook.png'
+import financas from '../../assets/financas.png'
+import cap from '../../assets/cap.png'
+import ButtonHome from '../../components/ButtonHome';
 
 const Home = () => {
     return (
         <div>
             <header>
-                <FaBars className='bars' />
+                <span className='icon-bars'>
+                    <FaBars className='bars' />
+                </span>
             </header>
             <main>
                 <figure>
                     <img src={logo} alt="logo da sumare" className='logo-sumare' />
                 </figure>
-                <h2>Teste 2</h2>
+                <h1>Sobre qual assunto deseja falar?</h1>
+                <div className='lista-assuntos'>
+                    <ButtonHome name="Financeiro" img= {financas} />
+                    <ButtonHome name="Acadêmico" img= {cap} />
+                    <ButtonHome name="AVA" img= {notebook} />
+                    <ButtonHome name="Problemas na matrícula" img= {notebook} />
+                </div>
+               
             </main>
         </div>
     )
