5f4ab8eac8cc9fc4f1489b42d41212ad9a889c50
millena-softsy <millena.souza@softsy.io>
2024-06-06 18:40:47 -0300

    modal feita

--- src/pages/Academic/CourseCancellation/index.jsx
+++ src/pages/Academic/CourseCancellation/index.jsx
@@ -1,10 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import DefaultButton from "../../../components/DefaultButton";
 import "./courseCancellation.css"
+import Modal from 'react-modal';
+import { useState } from "react";
 
 const CourseCancellation = () => {
 
-    const navegation = useNavigate()
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [hasPendingFees, setHasPendingFees] = useState(true);
+    const navigate = useNavigate();
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    const handleCancelCourse = () => {
+        if (hasPendingFees) {
+            alert("Você possui mensalidades pendentes.");
+            navigate("/academico/cancelamento-do-curso/cobrancas");
+        } else {
+            alert("Seguir");
+        }
+        closeModal();
+    };
 
     return (
         <main className="course-cancellation">
@@ -17,10 +39,26 @@ const CourseCancellation = () => {
                     text="Cancelar Curso"
                     backgroundColor="#DC143C"
                     color='#fff'
-                    onClick={() => navegation("/academico/cancelamento-do-curso/cobrancas")}
+                    onClick={openModal}
                 />
+
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Confirmar Cancelamento"
+                    className="modal-cancellation"
+                    overlayClassName="overlay"
+                >
+                    <div className='modal-content'>
+                        <span onClick={closeModal} className='close-modal-button'>&times;</span>
+                        <h2>Deseja mesmo cancelar o curso?</h2>
+                        <div className='modal-actions'>
+                            <button onClick={handleCancelCourse} className='modal-button confirm'>Sim</button>
+                            <button onClick={closeModal} className='modal-button cancel'>Não</button>
+                        </div>
+                    </div>
+                </Modal>
             </div>
-            
         </main>
     );
 
