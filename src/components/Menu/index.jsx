d35051beb02fee50a5faca2609a5f6ebe988203d
millena-softsy <millena.souza@softsy.io>
2024-06-19 19:11:35 -0300

    menu bar feito

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,17 @@
+// Menu.js
+import React from 'react';
+import './menu.css';
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <ul>
+                <li><a href="/perfil">Perfil</a></li>
+                <li><a href="/configuracoes">Configurações</a></li>
+                <li><a href="/">Sair</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
