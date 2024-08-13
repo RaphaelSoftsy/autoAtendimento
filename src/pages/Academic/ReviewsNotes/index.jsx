f3d3ff9f86e7b9c06de92471322ff2a9f72e5e97
millena-softsy <millena.souza@softsy.io>
2024-08-13 20:08:29 -0300

    Tela de Avaliações e Notas feita

--- src/pages/Academic/ReviewsNotes/index.jsx
+++ src/pages/Academic/ReviewsNotes/index.jsx
@@ -1,12 +1,131 @@
+import React, { useEffect, useState } from 'react';
+import DefaultButton from '../../../components/DefaultButton';
 import './reviewsNotes.css';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { useNavigate } from 'react-router-dom';
+import ReviewItem from '../../../components/ReviewItem';
+import { useRA } from '../../../contexts/RAContext';
+import axios from 'axios';
+import { url_base_local } from '../../../services/url_base';
 
 const ReviewsNotes = () => {
+    const [isSelecting, setIsSelecting] = useState(false);
+    const [selectedSubject, setSelectedSubject] = useState('');
+    const [selectedSubjects, setSelectedSubjects] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('');
+    const MySwal = withReactContent(Swal);
+    const navegation = useNavigate();
+    const { currentRA } = useRA();
+
+    const handleNext = () => {
+        if (!selectedSubject) {
+            MySwal.fire({
+                icon: 'info',
+                title: 'Erro',
+                text: 'Você não selecionou nenhuma disciplina.',
+                confirmButtonText: 'OK'
+            });
+        } else {
+            console.log(selectedSubject);
+            localStorage.setItem("disciplina-selecionada", selectedSubject);
+            navegation('numero-servico');
+        }
+    };
+
+    const handleButtonClick = () => {
+        if (!isSelecting) {
+            setIsSelecting(true);
+        } else {
+            handleNext();
+        }
+    };
+
+    const handleBackClick = () => {
+        setIsSelecting(false);
+        setSelectedSubject('');
+        setSelectedOption('');
+    };
+
+    const items = [
+        { nome: "Média", valor: "-" },
+        { nome: "Aulas previstas", valor: "24" },
+        { nome: "Aulas ministradas", valor: "0" },
+        { nome: "Limites de faltas", valor: "24" },
+        { nome: "Faltas acumuladas", valor: "0" },
+        { nome: "Presenças", valor: "100%" }
+    ];
+
+    useEffect(() => {
+        getDiscipline();
+    }, [currentRA]);
+
+    async function getDiscipline() {
+        MySwal.showLoading();
+
+        try {
+            const response = await axios.get(`${url_base_local}/disciplinaMatriculada/${currentRA.ra}`);
+            const data = response.data;
+
+            const formattedData = data.map((item, index) => ({
+                id: index + 1,
+                aluno: item.aluno,
+                name: item.nomeDisciplina
+            }));
+
+            setSelectedSubjects(formattedData);
+        } catch (error) {
+            console.error('Erro ao buscar disciplinas:', error);
+        }
+
+        MySwal.close();
+    }
+
+    const handleSelectChange = (e) => {
+        const selectedValue = e.target.value;
+        setSelectedOption(selectedValue);
+        setSelectedSubject(selectedValue);
+    };
 
     return (
         <main className="reviews-notes">
-            tela vai ser feita
-        </main>
-    )
-}
+            {/* <h1 className='title'>Notas e Revisões</h1> */}
 
-export default ReviewsNotes
+            {!isSelecting && (
+                <div className='select-discipline'>
+                    <select value={selectedOption} onChange={handleSelectChange} className="custom-select-medium">
+                        {selectedSubjects.map(item => (
+                            <option key={item.id} value={item.name} className='option-medium'>{item.name}</option>
+                        ))}
+                    </select>
+                </div>
+            )}
+
+            {isSelecting && (
+                <span className="back-arrow" onClick={handleBackClick}>&larr; Voltar</span>
+            )}
+
+            <ul className='list'>
+                {items.map((item, index) => (
+                    <ReviewItem
+                        key={index}
+                        isSelecting={isSelecting}
+                        subject={item.nome}
+                        selectedSubject={selectedSubject}
+                        onClick={() => setSelectedSubject(item.nome)}
+                        valor={item.valor}
+                    />
+                ))}
+            </ul>
+
+            <DefaultButton
+                text={isSelecting ? "Avançar" : "Solicitar Revisão de Nota"}
+                backgroundColor="var(--primary-light-blue)"
+                color='#fff'
+                onClick={handleButtonClick}
+            />
+        </main>
+    );
+};
+
+export default ReviewsNotes;
