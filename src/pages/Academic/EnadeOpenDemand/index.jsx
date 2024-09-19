404f043a7a63c90c86b4fe46071412be5b6eb2b8
millena-softsy <millena.souza@softsy.io>
2024-09-19 15:14:50 -0300

    ajustes e consumo das apis

--- src/pages/Academic/EnadeOpenDemand/index.jsx
+++ src/pages/Academic/EnadeOpenDemand/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
